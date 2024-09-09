200653a6425d5c309ab00421508a6ebbfad4261c
drewmercer-bl <drew.mercer@brandlive.com>
2024-09-09 08:55:06 -0700

    fix(main.tf): create dns auth records in aws

--- main.tf
+++ main.tf
@@ -3,8 +3,12 @@ locals {
   gcp_authorizations        = values(google_certificate_manager_dns_authorization.gcp_auth).*.id
   cloudflare_hostnames      = var.domain_cloud == "cloudflare" ? var.hostnames : []
   cloudflare_authorizations = values(google_certificate_manager_dns_authorization.cloudflare_auth).*.id
+  aws_hostnames             = var.domain_cloud == "aws" ? var.hostnames : []
+  aws_authorizations        = values(google_certificate_manager_dns_authorization.aws_auth).*.resource_record_name
   certificate_name          = var.certificate_name != "" ? var.certificate_name : "${var.name}"
 }
+
+#### GCP DNS Authorization ####
 
 # GCP Domain
 data "google_dns_managed_zone" "gcp_zone" {
@@ -45,20 +49,22 @@ resource "google_dns_record_set" "gcp_auth_cname" {
   rrdatas      = [google_certificate_manager_dns_authorization.gcp_auth[each.key].dns_resource_record[0].data]
 }
 
-# GCP Certificate
+# GCP Certificate with GCP DNS Authorization
 resource "google_certificate_manager_certificate" "gcp_certificate" {
   count = var.domain_cloud == "gcp" ? 1 : 0
 
   name        = "${replace(data.google_dns_managed_zone.gcp_zone[0].name, ".", "-")}-certificate"
   description = "${data.google_dns_managed_zone.gcp_zone[0].name} certificate"
   managed {
-    domains            = var.wildcard ? concat(local.gcp_hostnames, ["*.${var.domain}"]) : local.gcp_hostnames
+    domains            = local.gcp_hostnames
     dns_authorizations = local.gcp_authorizations
   }
   labels = {
     "terraform" : true
   }
 }
+
+### Cloudflare DNS Authorization ###
 
 # Cloudflare Domain
 data "cloudflare_zone" "domain" {
@@ -99,20 +105,77 @@ resource "cloudflare_record" "cloudflare_auth_cname" {
   proxied = false
 }
 
-# Cloudflare Certificate
+# GCP Certificate with Cloudflare DNS Authorization
 resource "google_certificate_manager_certificate" "cloudflare_certificate" {
   count = var.domain_cloud == "cloudflare" ? 1 : 0
 
   name        = local.certificate_name
   description = "${var.domain} certificate"
   managed {
-    domains            = var.wildcard ? concat(local.cloudflare_hostnames, ["*.${var.domain}"]) : local.cloudflare_hostnames
+    domains            = local.cloudflare_hostnames
     dns_authorizations = local.cloudflare_authorizations
   }
   labels = {
     "terraform" : true
   }
 }
+
+#### AWS DNS Authorization ####
+
+data "aws_route53_zone" "domain" {
+  count = var.domain_cloud == "aws" ? 1 : 0
+
+  name = var.domain
+}
+
+# AWS Randomized ID
+resource "random_string" "aws_rand" {
+  for_each = toset(local.aws_hostnames)
+
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# AWS DNS Authorization
+resource "google_certificate_manager_dns_authorization" "aws_auth" {
+  for_each = toset(local.aws_hostnames)
+
+  name        = "${replace(var.domain, ".", "-")}-dnsauth-${random_string.aws_rand[each.key].id}"
+  description = "DNS Authorization for ${var.domain}"
+  domain      = each.key
+  labels = {
+    "terraform" : true
+  }
+}
+
+# AWS DNS Authorization Record
+resource "aws_route53_record" "aws_auth_cname" {
+  for_each = toset(local.aws_hostnames)
+
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = google_certificate_manager_dns_authorization.aws_auth[each.key].dns_resource_record.0.name
+  type    = "CNAME"
+  ttl     = 300
+  records = [google_certificate_manager_dns_authorization.aws_auth[each.key].dns_resource_record.0.data]
+}
+
+# GCP Certificate with AWS DNS Authorization
+resource "google_certificate_manager_certificate" "aws_certificate" {
+  count = var.domain_cloud == "aws" ? 1 : 0
+
+  name        = local.certificate_name
+  description = "${var.domain} certificate"
+  managed {
+    domains            = local.aws_hostnames
+    dns_authorizations = local.aws_authorizations
+  }
+  labels = {
+    "terraform" : true
+  }
+}
+
+#### Certificate Map Entry ####
 
 # Certificate Map Entry (Primary, created if Default is true)
 resource "google_certificate_manager_certificate_map_entry" "default" {
@@ -126,6 +189,7 @@ resource "google_certificate_manager_certificate_map_entry" "default" {
   certificates = compact([
     try(google_certificate_manager_certificate.gcp_certificate[0].id, ""),
     try(google_certificate_manager_certificate.cloudflare_certificate[0].id, ""),
+    try(google_certificate_manager_certificate.aws_certificate[0].id, ""),
   ])
   matcher = "PRIMARY"
 }
@@ -142,6 +206,7 @@ resource "google_certificate_manager_certificate_map_entry" "certificate" {
   certificates = compact([
     try(google_certificate_manager_certificate.gcp_certificate[0].id, ""),
     try(google_certificate_manager_certificate.cloudflare_certificate[0].id, ""),
+    try(google_certificate_manager_certificate.aws_certificate[0].id, ""),
   ])
   hostname = var.hostnames[0]
 }
--- variables.tf
+++ variables.tf
@@ -9,19 +9,13 @@ variable "domain" {
 }
 
 variable "domain_cloud" {
-  description = "valid options: gcp, cloudflare"
+  description = "valid options: gcp, cloudflare, aws"
   type        = string
 }
 
 variable "hostnames" {
   description = "Hostnames to create certificates for"
   type        = list(string)
-}
-
-variable "wildcard" {
-  description = "Wildcard"
-  type        = bool
-  default     = false
 }
 
 variable "certificate_map" {
