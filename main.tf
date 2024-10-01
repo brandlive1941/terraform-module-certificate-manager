a29e5b02ad23900a3b927e3803d477e2d8fb2a88
drewmercer-bl <drew.mercer@brandlive.com>
2024-10-01 13:03:53 -0700

    fix(main.tf): simplified

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 locals {
-  gcp_hostnames             = var.domain_cloud == "gcp" ? var.hostnames : []
-  gcp_authorizations        = values(google_certificate_manager_dns_authorization.gcp_auth).*.id
   cloudflare_hostnames      = var.domain_cloud == "cloudflare" ? var.hostnames : []
   cloudflare_authorizations = values(google_certificate_manager_dns_authorization.cloudflare_auth).*.id
-  aws_hostnames             = var.domain_cloud == "aws" ? var.hostnames : []
-  aws_authorizations        = values(google_certificate_manager_dns_authorization.aws_auth).*.resource_record_name
+  gcp_hostnames             = var.domain_cloud == "gcp" ? var.hostnames : []
+  gcp_authorizations        = values(google_certificate_manager_dns_authorization.gcp_auth).*.id
   certificate_name          = var.certificate_name != "" ? var.certificate_name : "${var.name}"
 }
-
 #### GCP DNS Authorization ####
 
 # GCP Domain
@@ -19,7 +16,7 @@ data "google_dns_managed_zone" "gcp_zone" {
 
 # GCP Randomized ID
 resource "random_string" "gcp_rand" {
-  for_each = toset(local.gcp_hostnames)
+  for_each = toset(setsubtract(local.gcp_hostnames, ["*.${var.domain}"]))
 
   length  = 8
   special = false
@@ -28,7 +25,7 @@ resource "random_string" "gcp_rand" {
 
 # GCP DNS Authorization
 resource "google_certificate_manager_dns_authorization" "gcp_auth" {
-  for_each = toset(local.gcp_hostnames)
+  for_each = toset(setsubtract(local.gcp_hostnames, ["*.${var.domain}"]))
 
   name        = "${data.google_dns_managed_zone.gcp_zone[0].name}-dnsauth-${random_string.gcp_rand[each.key].id}"
   description = "DNS Authorization for ${data.google_dns_managed_zone.gcp_zone[0].dns_name}"
@@ -40,7 +37,7 @@ resource "google_certificate_manager_dns_authorization" "gcp_auth" {
 
 # GCP DNS Authorization Record
 resource "google_dns_record_set" "gcp_auth_cname" {
-  for_each = toset(local.gcp_hostnames)
+  for_each = toset(setsubtract(local.gcp_hostnames, ["*.${var.domain}"]))
 
   name         = google_certificate_manager_dns_authorization.gcp_auth[each.key].dns_resource_record[0].name
   managed_zone = data.google_dns_managed_zone.gcp_zone[0].name
@@ -75,7 +72,7 @@ data "cloudflare_zone" "domain" {
 
 # Cloudflare Randomized ID
 resource "random_string" "cf_rand" {
-  for_each = toset(local.cloudflare_hostnames)
+  for_each = toset(setsubtract(local.cloudflare_hostnames, ["*.${var.domain}"]))
 
   length  = 8
   special = false
@@ -84,7 +81,7 @@ resource "random_string" "cf_rand" {
 
 # Cloudflare DNS Authorization
 resource "google_certificate_manager_dns_authorization" "cloudflare_auth" {
-  for_each = toset(local.cloudflare_hostnames)
+  for_each = toset(setsubtract(local.cloudflare_hostnames, ["*.${var.domain}"]))
 
   name        = "${replace(var.domain, ".", "-")}-dnsauth-${random_string.cf_rand[each.key].id}"
   description = "DNS Authorization for ${var.domain}"
@@ -96,7 +93,7 @@ resource "google_certificate_manager_dns_authorization" "cloudflare_auth" {
 
 # Cloudflare DNS Authorization Record
 resource "cloudflare_record" "cloudflare_auth_cname" {
-  for_each = toset(local.cloudflare_hostnames)
+  for_each = toset(setsubtract(local.cloudflare_hostnames, ["*.${var.domain}"]))
 
   zone_id = data.cloudflare_zone.domain[0].id
   name    = google_certificate_manager_dns_authorization.cloudflare_auth[each.key].dns_resource_record.0.name
@@ -120,61 +117,6 @@ resource "google_certificate_manager_certificate" "cloudflare_certificate" {
   }
 }
 
-#### AWS DNS Authorization ####
-
-data "aws_route53_zone" "domain" {
-  count = var.domain_cloud == "aws" ? 1 : 0
-
-  name = var.domain
-}
-
-# AWS Randomized ID
-resource "random_string" "aws_rand" {
-  for_each = toset(local.aws_hostnames)
-
-  length  = 8
-  special = false
-  upper   = false
-}
-
-# AWS DNS Authorization
-resource "google_certificate_manager_dns_authorization" "aws_auth" {
-  for_each = toset(local.aws_hostnames)
-
-  name        = "${replace(var.domain, ".", "-")}-dnsauth-${random_string.aws_rand[each.key].id}"
-  description = "DNS Authorization for ${var.domain}"
-  domain      = each.key
-  labels = {
-    "terraform" : true
-  }
-}
-
-# AWS DNS Authorization Record
-resource "aws_route53_record" "aws_auth_cname" {
-  for_each = toset(local.aws_hostnames)
-
-  zone_id = aws_route53_zone.domain.zone_id
-  name    = google_certificate_manager_dns_authorization.aws_auth[each.key].dns_resource_record.0.name
-  type    = "CNAME"
-  ttl     = 300
-  records = [google_certificate_manager_dns_authorization.aws_auth[each.key].dns_resource_record.0.data]
-}
-
-# GCP Certificate with AWS DNS Authorization
-resource "google_certificate_manager_certificate" "aws_certificate" {
-  count = var.domain_cloud == "aws" ? 1 : 0
-
-  name        = local.certificate_name
-  description = "${var.domain} certificate"
-  managed {
-    domains            = local.aws_hostnames
-    dns_authorizations = local.aws_authorizations
-  }
-  labels = {
-    "terraform" : true
-  }
-}
-
 #### Certificate Map Entry ####
 
 # Certificate Map Entry (Primary, created if Default is true)
@@ -189,7 +131,6 @@ resource "google_certificate_manager_certificate_map_entry" "default" {
   certificates = compact([
     try(google_certificate_manager_certificate.gcp_certificate[0].id, ""),
     try(google_certificate_manager_certificate.cloudflare_certificate[0].id, ""),
-    try(google_certificate_manager_certificate.aws_certificate[0].id, ""),
   ])
   matcher = "PRIMARY"
 }
@@ -206,7 +147,6 @@ resource "google_certificate_manager_certificate_map_entry" "certificate" {
   certificates = compact([
     try(google_certificate_manager_certificate.gcp_certificate[0].id, ""),
     try(google_certificate_manager_certificate.cloudflare_certificate[0].id, ""),
-    try(google_certificate_manager_certificate.aws_certificate[0].id, ""),
   ])
   hostname = var.hostnames[0]
 }
